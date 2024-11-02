61ad31823d02a6b8a6e04de76169596751a76770
SnappyWave <showerpool@gmail.com>
2024-11-02 19:27:45 +0300

    Initial commit

---
+++ src/main/java/com/xory/france/FranceApplication.java
@@ -0,0 +1,13 @@
+package com.xory.france;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FranceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FranceApplication.class, args);
+	}
+
+}
